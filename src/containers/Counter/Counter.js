2c4478219504213ecfd42b28724c1b4da14b55ef
Paul <p.matchett80@gmail.com>
2018-05-21 17:26:20 +0100

    replaced div wrapper with React Fragment

--- src/containers/Counter/Counter.js
+++ src/containers/Counter/Counter.js
@@ -8,7 +8,7 @@ import CounterOutput from '../../components/CounterOutput/CounterOutput';
 class Counter extends Component {
     render () {
         return (
-            <div>
+            <React.Fragment>
                 <CounterOutput value={this.props.ctr} />
                 <CounterControl label="Increment" clicked={this.props.onIncrementCounter} />
                 <CounterControl label="Decrement" clicked={this.props.onDecrementCounter}  />
@@ -17,18 +17,22 @@ class Counter extends Component {
                 <hr />
                 <button onClick={() => this.props.onStoreResult(this.props.ctr)}>Store Result</button>
                 <ul>
-                    {this.props.storedResults.map(strResult => (
-                        <li key={strResult.id} onClick={() => this.props.onDeleteResult(strResult.id)}>{strResult.value}</li>
-                    ))}
+                    {
+                        this.props.storedResults.map(strResult => 
+                            (
+                                <li key={strResult.id} 
+                                    onClick={() => this.props.onDeleteResult(strResult.id)}>{strResult.value}</li>
+                            )
+                        )
+                    }
                 </ul>
-            </div>
+            </React.Fragment>
         );
     }
 }
 
 const mapStateToProps = state => {
     return {
-
         ctr: state.ctr.counter,
         storedResults: state.res.results
     }
